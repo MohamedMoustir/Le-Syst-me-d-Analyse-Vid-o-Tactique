940d3d991090bce4d81ce831d0171ce6063d1be9
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:46 +0100

    feat(domain): implement PositionData entity for tracking

--- src/main/java/com/savt/backend/domain/entity/PositionData.java
+++ src/main/java/com/savt/backend/domain/entity/PositionData.java
@@ -1,4 +1,22 @@
 package com.savt.backend.domain.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "tracking_data")
 public class PositionData {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String videoId;
+
+    private Long frameNumber;
+    private Float x;
+    private Float y;
+    private Float speed;
+
+    private String joueurId;
 }
