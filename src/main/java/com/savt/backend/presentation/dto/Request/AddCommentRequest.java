ee824539180d83bcb0b13b189ef55cd891ff1a65
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:58:06 +0100

    feat(dto): add requests for creating events and comments

--- src/main/java/com/savt/backend/presentation/dto/Request/AddCommentRequest.java
+++ src/main/java/com/savt/backend/presentation/dto/Request/AddCommentRequest.java
@@ -1,4 +1,11 @@
 package com.savt.backend.presentation.dto.Request;
 
+
+import lombok.Data;
+
+@Data
 public class AddCommentRequest {
+    private String auteurNom;
+    private String contenu;
+    private Long timestampVideo;
 }
--- src/main/java/com/savt/backend/presentation/dto/Request/AddEventRequest.java
+++ src/main/java/com/savt/backend/presentation/dto/Request/AddEventRequest.java
@@ -1,4 +1,10 @@
 package com.savt.backend.presentation.dto.Request;
 
+import lombok.Data;
+
+@Data
 public class AddEventRequest {
+    private Long timestampVideo;
+    private String type;
+    private String description;
 }
