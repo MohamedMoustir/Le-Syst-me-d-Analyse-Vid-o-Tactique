2af1c3b768b09637bd540ebf79f9cf936103d128
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:59 +0100

    feat(dto): add video upload request DTO

--- src/main/java/com/savt/backend/presentation/dto/Request/VideoUploadRequest.java
+++ src/main/java/com/savt/backend/presentation/dto/Request/VideoUploadRequest.java
@@ -1,4 +1,12 @@
 package com.savt.backend.presentation.dto.Request;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class VideoUploadRequest {
+    private String titre;
+    private String uploaderId;
 }
+
+
