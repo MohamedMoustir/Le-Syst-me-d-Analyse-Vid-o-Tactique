7b206dccb2f7e9da0ec9e111c367cc15ea1fd703
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:32 +0100

    feat(domain): implement Utilisateur entity

--- src/main/java/com/savt/backend/domain/entity/Utilisateur.java
+++ src/main/java/com/savt/backend/domain/entity/Utilisateur.java
@@ -1,4 +1,20 @@
 package com.savt.backend.domain.entity;
 
+import com.savt.backend.domain.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
+@Data
+@Builder
 public class Utilisateur {
+    @Id
+    private String id;
+
+    private String nom;
+    private String email;
+    private String password;
+    private Role role;
 }
