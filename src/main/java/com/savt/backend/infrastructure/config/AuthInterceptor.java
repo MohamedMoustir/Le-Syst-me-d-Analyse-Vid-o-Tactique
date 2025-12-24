579c9074cff8e41d40c0ff95811958ea3e2479b3
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:25 +0100

    refactor(infra): move AuthInterceptor to infrastructure

--- src/main/java/com/savt/backend/infrastructure/config/AuthInterceptor.java
+++ src/main/java/com/savt/backend/infrastructure/config/AuthInterceptor.java
@@ -1,4 +1,4 @@
-package com.savt.backend.config;
+package com.savt.backend.infrastructure.config;
 
 import com.savt.backend.shared.exception.ForbiddenException;
 import com.savt.backend.shared.exception.UnauthorizedException;
